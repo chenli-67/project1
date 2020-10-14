7dacb6c36357b50167c05ea92dd3201368eb2c7b
CAT <516379822@qq.com>
2020-10-14 10:52:58 +0800

    主页完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -51,7 +51,7 @@ $(function () {
         }
         layer.msg('登陆成功');
         localStorage.setItem('token', res.token)
-        // location.href = 'index.html'
+        location.href = 'index.html'
       }
     })
   })
